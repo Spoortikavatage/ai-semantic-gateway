4b06a4adce533c448557bbc8794c2b6d78a570b6
abhijeetbaloji <abhijeetbalojipesu@gmail.com>
2026-05-22 10:37:37 +0530

    Add cosine similarity search for semantic caching

---
+++ services/semantic-gateway/src/main/java/com/semantic/gateway/util/SimilarityUtil.java
@@ -0,0 +1,22 @@
+
+package com.semantic.gateway.util;
+
+import java.util.List;
+
+public class SimilarityUtil {
+
+  public static double cosineSimilarity(List<Double> v1, List<Double> v2) {
+
+    double dotProduct = 0.0;
+    double norm1 = 0.0;
+    double norm2 = 0.0;
+
+    for (int i = 0; i < v1.size(); i++) {
+      dotProduct += v1.get(i) * v2.get(i);
+      norm1 += Math.pow(v1.get(i), 2);
+      norm2 += Math.pow(v2.get(i), 2);
+    }
+
+    return dotProduct / (Math.sqrt(norm1) * Math.sqrt(norm2));
+  }
+}
